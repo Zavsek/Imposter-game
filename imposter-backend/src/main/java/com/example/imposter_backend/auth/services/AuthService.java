6b622e14d82f2cb236ebc5c7491e935fb3326d90
ZAvsek <filip.zavsek@student.um.si>
2026-03-31 00:44:14 +0200

    refactored auth service

--- imposter-backend/src/main/java/com/example/imposter_backend/auth/services/AuthService.java
+++ imposter-backend/src/main/java/com/example/imposter_backend/auth/services/AuthService.java
@@ -32,8 +32,14 @@ public class AuthService {
         String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
         return hashedPassword;
     }
+
+    public void checkPassword(String password, String hashedPassword){
+        boolean authenticated =  BCrypt.checkpw(password, hashedPassword);
+        if(!authenticated) throw new IllegalArgumentException("Credential do not match");
+    }
     @Transactional
     public String register (RegistrationRequestDTO request){
+
         if(authRepository.existsByEmail(request.email())){
             throw new IllegalArgumentException("Email is already registered");
         }
@@ -41,37 +47,48 @@ public class AuthService {
             throw new IllegalArgumentException("Email is not valid");
         }
 
-        Auth auth = new Auth();
-        auth.setEmail(request.email());
-        auth.setHashedPassword(hashPassword(request.password()));
-        
-        authRepository.save(auth);
-
-        Player player = new Player();
-        player.setUsername(request.username());
-        player.setAuth(auth);
-        
-        playerRepository.save(player);
+        Auth auth = createAndSaveAuthInstance(request.email(), request.password());
+        createAndSavePlayerInstance(request.username(), auth);
 
         return "Registration Successfull";
     }
-     public LoginResponseDTO login(LoginRequestDTO request){
+
+    private void createAndSavePlayerInstance(String username, Auth auth) {
+        Player player = new Player();
+        player.setUsername(username);
+        player.setAuth(auth);
+        playerRepository.save(player);
+    }
+
+    private Auth createAndSaveAuthInstance(String email, String password) {
+        Auth auth = new Auth();
+        auth.setEmail(email);
+        auth.setHashedPassword(hashPassword(password));
+        authRepository.save(auth);
+        return auth;
+    }
+
+    public boolean checkEmailValidity(String email){
+        String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+        return Pattern.compile(regexPattern).matcher(email).matches();
+    }
+
+    // ------------------------------------------------------------------------------------------
+
+    public LoginResponseDTO login(LoginRequestDTO request){
             Auth auth= authRepository.findByEmail(request.email()).orElseThrow(()-> new IllegalArgumentException("Credential not found"));
 
-                String requestPass = request.password();
-                String userPass = auth.getHashedPassword();
-                boolean authenticated = BCrypt.checkpw(requestPass, userPass);
-                if(!authenticated) throw new IllegalArgumentException("Credential do not match");
-                Player player = playerRepository.findByAuth(auth).orElseThrow(()-> new IllegalStateException("Player not found for auth record"));
+
+                checkPassword(request.password(), auth.getHashedPassword());
+                Player player = playerRepository.findByAuth(auth)
+                        .orElseThrow(()-> new IllegalStateException("Player not found for auth record"));
                 String token = jwtService.generateToken(player);
 
-                return new LoginResponseDTO(player.getId(), player.getUsername(), token, LocalDateTime.now());
-
-
+                return new LoginResponseDTO(player.getId(),
+                        player.getUsername(),
+                        token,
+                        LocalDateTime.now());
         }
 
-        public boolean checkEmailValidity(String email){
-            String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-             return Pattern.compile(regexPattern).matcher(email).matches();
-        }
+
 }
